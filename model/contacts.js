f2afd4cf26c82103388e0d8033a443a45861349c
Oksana Kochenko <oksanakochenkoo@gmail.com>
2021-05-13 22:19:45 +0300

    added get, post & get by id

---
+++ model/contacts.js
@@ -0,0 +1,34 @@
+// const fs = require('fs/promises')
+// const contacts = require('./contacts.json')
+const db = require('./db')
+const { v4: uuidv4 } = require('uuid')
+
+const listContacts = async () => {
+  return db.get('contacts').value()
+}
+
+const getContactById = async (id) => {
+  return db.get('contacts').find({ id }).value()
+}
+
+const removeContact = async (id) => {}
+
+const addContact = async (body) => {
+  const id = uuidv4()
+  const record = {
+    id,
+    ...body,
+  }
+  db.get('contacts').push(record).write()
+  return record
+}
+
+const updateContact = async (id, body) => {}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,52 @@
 const express = require('express')
 const router = express.Router()
+const Contacts = require('../../model/contacts')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await Contacts.listContacts()
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: { contacts },
+    })
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const contact = await Contacts.getContactById(req.params.id)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: { contact },
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not found',
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact = await Contacts.addContact(req.body)
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { contact },
+    })
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
